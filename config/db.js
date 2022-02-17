b8bda943c6a201166eb7e45b2233b60f40e928bf
Stella Massa Rebolledo <72529414+StellaMassaRebolledo@users.noreply.github.com>
2022-02-17 00:43:38 -0500

    mongo2

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
+//To start to connect my db. This is the address to my cluster
+
 module.exports={
     "DB_CONNECTION" : "mongodb+srv://SMassa:HtH3HdJywrd1XWno@cluster002.ayqg5.mongodb.net/comp229002"
 }
